9c14132fd252d2aa670c5d36e2110dc08a5a8f0d
Alvin <ypochien@gmail.com>
2015-10-26 23:54:40 +0800

    fixed to pass TestCase2 Scenario: 第一集買了一本，第二集也買了一本，價格應為100*2*0.95=190 寫得太開心，忘記commit Test Case 2 失敗的狀態

--- PotterShoppingCart/PotterShoppingCart.py
+++ PotterShoppingCart/PotterShoppingCart.py
@@ -10,5 +10,5 @@ class PotterShoppingCart:
 
     def calculate_total_amount(self):
         total_amount = sum([book['Cost'] for book in self.books])
-        discount_map = {1: 1, 2: 0.95}
-        return total_amount * discount_map[len(self.books)]
+        discount_map = {1: 1, 2: 0.95, 3: 0.9}
+        return total_amount * discount_map.get(len(self.books), 1)
