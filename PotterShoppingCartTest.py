3ffda9d306672a4c6afc8a0983a1ccfa9f5ec891
Alvin <ypochien@gmail.com>
2015-10-26 23:54:00 +0800

    added Test Case 3 - failed

--- PotterShoppingCartTest.py
+++ PotterShoppingCartTest.py
@@ -37,6 +37,22 @@ class PotterShoppingCartTEST(unittest.TestCase):
         expected = 190
         self.assertEqual(expected, actual)
 
+    def test_Buy_Ep1_Ep2_Ep3_Should_Be_270(self):
+        """
+        Scenario: 一二三集各買了一本，價格應為100*3*0.9=270
+        :return:
+        """
+        target = PotterShoppingCart.PotterShoppingCart()
+        target.pick_up({"Name": "HollyPoter 1", "Cost": 100})
+        target.pick_up({"Name": "HollyPoter 2", "Cost": 100})
+        target.pick_up({"Name": "HollyPoter 3", "Cost": 100})
+
+        # Act
+        actual = target.calculate_total_amount()
+
+        expected = 270
+        self.assertEqual(expected, actual)
+
 
 if __name__ == '__main__':
     unittest.main()
