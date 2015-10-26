12b450dedfda07e3b7faa5a87898f65b1ccd7a5a
Alvin <ypochien@gmail.com>
2015-10-26 23:57:44 +0800

    Added Test Case4 and fail Scenario: Scenario: 一二三四集各買了一本，價格應為100*4*0.8=320

--- PotterShoppingCartTest.py
+++ PotterShoppingCartTest.py
@@ -53,6 +53,21 @@ class PotterShoppingCartTEST(unittest.TestCase):
         expected = 270
         self.assertEqual(expected, actual)
 
+    def test_Buy_Ep1_Ep2_Ep3_Ep4_Should_Be_320(self):
+        """
+        Scenario: 一二三四集各買了一本，價格應為100*4*0.8=320
+                :return:
+        """
+        target = PotterShoppingCart.PotterShoppingCart()
+        target.pick_up({"Name": "HollyPoter 1", "Cost": 100})
+        target.pick_up({"Name": "HollyPoter 2", "Cost": 100})
+        target.pick_up({"Name": "HollyPoter 3", "Cost": 100})
+
+        # Act
+        actual = target.calculate_total_amount()
+
+        expected = 320
+        self.assertEqual(expected, actual)
 
 if __name__ == '__main__':
     unittest.main()
