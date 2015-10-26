d5c5a97fa564ec17d54e3a95f2db4274e83cd889
Alvin <ypochien@gmail.com>
2015-10-27 00:40:24 +0800

    fixed Test Case 6 Scenario: 一二集各買了一本，第三集買了兩本，價格應為100*3*0.9 + 100 = 370

--- PotterShoppingCart/PotterShoppingCart.py
+++ PotterShoppingCart/PotterShoppingCart.py
@@ -1,6 +1,5 @@
 # coding=utf-8
 
-
 class PotterShoppingCart:
     def __init__(self):
         self.books = []
@@ -9,6 +8,18 @@ class PotterShoppingCart:
         self.books.append(book)
 
     def calculate_total_amount(self):
-        total_amount = sum([book['Cost'] for book in self.books])
+        temp_books = self.books
+        discount_amount = 0.0
+        while len(temp_books) > 0:
+            distinct_books = {v['Name']: v for v in temp_books}.values()
+            discount_amount += self.__calculate_discount_by_books(distinct_books)
+            for x in distinct_books:
+                temp_books.remove(x)
+
+        return discount_amount
+
+    @staticmethod
+    def __calculate_discount_by_books(distinct_books):
+        total_amount = sum([book['Cost'] for book in distinct_books])
         discount_map = {1: 1, 2: 0.95, 3: 0.9, 4: 0.8, 5: 0.75}
-        return total_amount * discount_map.get(len(self.books), 1)
+        return total_amount * discount_map.get(len(distinct_books), 1)
